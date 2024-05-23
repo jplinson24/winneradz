5c829f5ecd01576f573e5a4fafa619d6057736d2
JP <jplinsonc24@gmail.com>
2024-05-23 09:21:21 +0800

    Updated Javascript Files

--- assets/js/formcontainer.js
+++ assets/js/formcontainer.js
@@ -1,13 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get references to the button and form container
-    var getStartedButton = document.getElementById("getStartedButton");
-    var formContainer = document.getElementById("formContainer");
-  
-    // Add click event listener to the button
-    getStartedButton.addEventListener("click", function(event) {
+  // Get references to the button, form container, and close button
+  var getStartedButton = document.getElementById("getStartedButton");
+  var formContainer = document.getElementById("formContainer");
+  var closeButton = document.getElementById("closeButton");
+
+  // Initially hide the form container
+  formContainer.style.display = "none";
+
+  // Add click event listener to the button
+  getStartedButton.addEventListener("click", function(event) {
       // Prevent default link behavior
       event.preventDefault();
       // Show the form container
       formContainer.style.display = "block";
-    });
   });
+
+  // Add click event listener to the close button
+  closeButton.addEventListener('click', function() {
+      // Hide the form container
+      formContainer.style.display = 'none';
+  });
+});
