5ac35fef2f9e89d96a7d1bd556be5873cdc9e0b6
Your Name <youremail@example.com>
2024-06-26 10:39:23 +0800

    all buttons work

--- assets/js/formcontainer.js
+++ assets/js/formcontainer.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Get references to the button, form container, and close button
-  var getStartedButton = document.getElementById("getStartedButton");
+  // Get references to all buttons with the class "show-form-button"
+  var buttons = document.querySelectorAll(".show-form-button");
   var formContainer = document.getElementById("formContainer");
   var closeButton = document.getElementById("closeButton");
 
   // Initially hide the form container
   formContainer.style.display = "none";
 
-  // Add click event listener to the button
-  getStartedButton.addEventListener("click", function(event) {
-      // Prevent default link behavior
-      event.preventDefault();
-      // Show the form container
-      formContainer.style.display = "block";
+  // Add click event listener to each button
+  buttons.forEach(function(button) {
+      button.addEventListener("click", function(event) {
+          // Prevent default link behavior
+          event.preventDefault();
+          // Show the form container
+          formContainer.style.display = "block";
+      });
   });
 
   // Add click event listener to the close button
