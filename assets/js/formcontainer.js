f6a0d31b2dd9f9f801075f2585c872a7a00bb933
jplinson24 <jplinsonc24@gmail.com>
2024-06-29 10:01:24 +0800

    updated

--- assets/js/formcontainer.js
+++ assets/js/formcontainer.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Get references to all buttons with the class "show-form-button"
   var buttons = document.querySelectorAll(".show-form-button");
   var formContainer = document.getElementById("formContainer");
   var closeButton = document.getElementById("closeButton");
-  var orderFormButton = document.getElementById("orderFormButton");
 
   // Initially hide the form container
   formContainer.style.display = "none";
@@ -11,28 +9,34 @@ document.addEventListener("DOMContentLoaded", function () {
   // Add click event listener to each button
   buttons.forEach(function (button) {
     button.addEventListener("click", function (event) {
-      // Prevent default link behavior
       event.preventDefault();
-      // Show the form container
       formContainer.style.display = "block";
     });
   });
 
   // Add click event listener to the close button
   closeButton.addEventListener("click", function () {
-    // Hide the form container
     formContainer.style.display = "none";
   });
 
-  // Add click event listener to the "Order Form" button
-  orderFormButton.addEventListener("click", function (event) {
-    // Prevent default form submission behavior
-    event.preventDefault();
+  // Add click event listener to the document
+  document.addEventListener("click", function (event) {
+    // Check if the clicked element is outside the form container and not the show-form-button
+    if (
+      !formContainer.contains(event.target) &&
+      !event.target.classList.contains("show-form-button")
+    ) {
+      formContainer.style.display = "none";
+    }
+  });
 
-    // Get the form action URL from the button attribute
-    var formAction = orderFormButton.getAttribute("formaction");
+  // Prevent event propagation when clicking inside the form container
+  formContainer.addEventListener("click", function (event) {
+    event.stopPropagation();
+  });
 
-    // Open the Google Form in a new tab
-    window.open(formAction, "_blank");
+  // Add scroll event listener to the window
+  window.addEventListener("scroll", function () {
+    formContainer.style.display = "none";
   });
 });
